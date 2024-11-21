5107d11098968c3cbb253ad44749ae382884bfd4
Igor <smutcpp@gmail.com>
2024-11-21 15:18:45 +0100

    added ray and players structs, init raycaster.c file

--- include/cub3d.h
+++ include/cub3d.h
@@ -26,4 +26,32 @@ typedef struct s_cub3d
 	void	*win;
 }	t_cub3d;
 
+typedef struct s_player
+{
+	double	posx;
+	double	posy;
+	double	dirx;
+	double	diry;
+	double	planex;
+	double	planey;
+} t_player;
+
+typedef struct s_ray
+{
+	double	raydirx;
+	double	raydiry;
+	double	sidedistx;
+	double	sidedisty;
+	double	deltadistx;
+	double	deltadisty;
+	double	perpwalldist;
+	int			mapx;
+	int			mapy;
+	int			stepx;
+	int			stepy;
+	int			hit;
+	int			side;
+
+} t_ray;
+
 #endif

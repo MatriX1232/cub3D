341721e9ae34e97a6e362d157f33347e46d1da40
msolinsk <msolinsk@student.42warsaw.pl>
2024-11-21 22:23:04 +0100

    Added custom put_img_to_img

--- src/ft_logs.c
+++ src/ft_logs.c
@@ -6,7 +6,7 @@
 /*   By: msolinsk <msolinsk@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 14:16:38 by msolinsk          #+#    #+#             */
-/*   Updated: 2024/11/21 19:14:32 by msolinsk         ###   ########.fr       */
+/*   Updated: 2024/11/21 19:27:49 by msolinsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,11 @@ void	ft_log(char *str, char *str_optional, int level)
 	if (level == 0)
 		printf("[ %sSUCCESS%s ] %s", GREEN, END, str);
 	else if (level == 1)
-		printf("[ %sINFO%s ] %s", BLUE, END, str);
+		printf("[ %sINFO%s ]    %s", BLUE, END, str);
 	else if (level == 2)
 		printf("[ %sWARNING%s ] %s", YELLOW, END, str);
 	else if (level == 3)
-		printf("[ %sERROR%s ] %s", RED, END, str);
+		printf("[ %sERROR%s ]   %s", RED, END, str);
 	if (str_optional != NULL)
 		printf(" | %s\n", str_optional);
 	else
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: msolinsk <msolinsk@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 12:32:15 by msolinsk          #+#    #+#             */
-/*   Updated: 2024/11/21 14:17:50 by msolinsk         ###   ########.fr       */
+/*   Updated: 2024/11/21 22:21:34 by msolinsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 int ft_exit(t_cub3d *cub3d)
 {
+	ft_free_sprites(cub3d);
 	mlx_destroy_window(cub3d->mlx, cub3d->win);
 	mlx_destroy_display(cub3d->mlx);
 	free(cub3d->mlx);
@@ -32,21 +33,34 @@ int	ft_key_hook(int keycode, t_cub3d *cub3d)
 int	main(int argc, char **argv)
 {
 	(void)argc;
-	(void)argv;
 
 	t_cub3d cub3d;
 
 	void	*mlx = mlx_init();
 	void	*win = mlx_new_window(mlx, 800, 600, "CUB3D");
+	ft_log("MLX and WIN initialized", NULL, 1);
 
 	cub3d.mlx = mlx;
 	cub3d.win = win;
 	cub3d.sprites = ft_load_sprites(mlx);
+	if (!cub3d.sprites)
+		return (ft_log("Sprites failed to load", NULL, 3), 1);
+	cub3d.map = ft_load_map(argv[1]);
+	if (!cub3d.map)
+		return (ft_log("Map failed to load", NULL, 3), 1);
 
 	mlx_hook(cub3d.win, ON_DESTROY, 0, ft_exit, &cub3d);
 	mlx_key_hook(cub3d.win, ft_key_hook, &cub3d);
 
-	mlx_put_image_to_window(mlx, win, cub3d.sprites[0]->img, 0, 0);
+	t_sprite *sprite = xpm_load_image(mlx, "textures/42warsaw.xpm");
+	int	i = 0;
+	while (cub3d.sprites[i])
+	{
+		put_img_to_img(sprite, cub3d.sprites[i], i * 70, 0);
+		i++;
+	}
+	mlx_put_image_to_window(mlx, win, sprite->img, 0, 0);
+
 
 	mlx_loop(mlx);
 

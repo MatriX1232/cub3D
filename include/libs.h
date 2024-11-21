b16eab5c9b394052f0d95b953825831ebf3c7b01
msolinsk <msolinsk@student.42warsaw.pl>
2024-11-21 15:19:28 +0100

    Added logs | Added textures loading

--- include/libs.h
+++ include/libs.h
@@ -1,34 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
+/*   libs.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msolinsk <msolinsk@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/21 13:26:15 by msolinsk          #+#    #+#             */
-/*   Updated: 2024/11/21 14:23:00 by msolinsk         ###   ########.fr       */
+/*   Created: 2024/11/21 14:17:01 by msolinsk          #+#    #+#             */
+/*   Updated: 2024/11/21 14:17:15 by msolinsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB3D_H
-# define CUB3D_H
+#ifndef LIBS_H
+# define LIBS_H
 
-# include <mlx.h>
-# include <mlx_int.h>
-# include "structs.h"
-# include <stdio.h>
-
-# define KEY_ESC 65307
-# define ON_DESTROY 17
-
-//	MAIN.C
-void	ft_cprint(char *color, char *str);
-
-//	FT_IMAGES.C
-void	ft_print_img_info(t_sprite *img);
-t_sprite	**ft_load_sprites(void *mlx);
-
-//	FT_LOGS.C
-void	ft_log(char *str, int level);
+#include "../include/mlx.h"
+#include "../include/mlx_int.h"
+#include "../include/cub3d.h"
+#include "../include/structs.h"
+#include "../include/colors.h"
+#include "../include/libft.h"
+#include <stdio.h>
 
 #endif
---
+++ src/ft_logs.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_logs.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msolinsk <msolinsk@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/21 14:16:38 by msolinsk          #+#    #+#             */
+/*   Updated: 2024/11/21 15:12:28 by msolinsk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/cub3d.h"
+#include "../include/libs.h"
+
+void	ft_cprint(char *color, char *str)
+{
+	write(1, color, ft_strlen(color));
+	write(1, str, ft_strlen(str));
+	write(1, END, ft_strlen(END));
+}
+
+void	ft_log(char *str, int level)
+{
+	if (level == 0)
+		printf("[ %sSUCCESS%s ] %s", GREEN, END, str);
+	else if (level == 1)
+		printf("[ %sINFO%s ] %s", BLUE, END, str);
+	else if (level == 2)
+		printf("[ %sWARNING%s ] %s", YELLOW, END, str);
+	else if (level == 3)
+		printf("[ %sERROR%s ] %s", RED, END, str);
+}

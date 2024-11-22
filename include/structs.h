ada45f2683fd23aa08b62871a2db39c43bb3726b
igordotdev <igordomagalo@gmail.com>
2024-11-22 15:52:25 +0100

    yolo commit, will be a merge conflict lmao

--- include/structs.h
+++ include/structs.h
@@ -25,11 +25,53 @@ typedef struct s_sprite
 	int		endian;
 }	t_sprite;
 
+typedef struct s_map
+{
+	int		**map;
+	int		w;
+	int		h;
+} t_map;
+
+typedef struct s_player
+{
+	double	posx;
+	double	posy;
+	double	dirx;
+	double	diry;
+	double	planex;
+	double	planey;
+} t_player;
+
 typedef struct s_cub3d
 {
 	void		*mlx;
 	void		*win;
+	int			w;
+	int			h;
+
+	t_map		map;
+	t_player	player;
 	t_sprite	**sprites;
 }	t_cub3d;
+
+
+typedef struct s_ray
+{
+	double	camerax;
+	double	raydirx;
+	double	raydiry;
+	double	sidedistx;
+	double	sidedisty;
+	double	deltadistx;
+	double	deltadisty;
+	double	perpwalldist;
+	int			mapx;
+	int			mapy;
+	int			stepx;
+	int			stepy;
+	int			hit;
+	int			side;
+
+} t_ray;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -35,19 +35,21 @@ int	main(int argc, char **argv)
 	(void)argv;
 
 	t_cub3d cub3d;
+	cub3d.h = 600;
+	cub3d.w = 800;
+
 
 	void	*mlx = mlx_init();
-	void	*win = mlx_new_window(mlx, 800, 600, "CUB3D");
-
+	void	*win = mlx_new_window(mlx, cub3d.w, cub3d.h, "CUB3D");
+	
 	cub3d.mlx = mlx;
 	cub3d.win = win;
 	cub3d.sprites = ft_load_sprites(mlx);
 
 	mlx_hook(cub3d.win, ON_DESTROY, 0, ft_exit, &cub3d);
 	mlx_key_hook(cub3d.win, ft_key_hook, &cub3d);
-
-	mlx_put_image_to_window(mlx, win, cub3d.sprites[0]->img, 0, 0);
-
+	// mlx_put_image_to_window(mlx, win, cub3d.sprites[0]->img, 0, 0);
+	raycaster(&cub3d);
 	mlx_loop(mlx);
 
 	return (0);

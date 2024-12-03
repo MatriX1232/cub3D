8e62bdeac88f28c3957c613970a3f682d125e9b3
msolinsk <msolinsk@student.42warsaw.pl>
2024-12-03 18:08:36 +0100

    Fixed norminette in font_renderer.c

--- src/font_renderer.c
+++ src/font_renderer.c
@@ -6,7 +6,7 @@
 /*   By: msolinsk <msolinsk@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 21:39:29 by msolinsk          #+#    #+#             */
-/*   Updated: 2024/12/03 16:51:11 by msolinsk         ###   ########.fr       */
+/*   Updated: 2024/12/03 18:08:05 by msolinsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,46 @@
 #include <X11/Xlib.h>
 #include <X11/Xft/Xft.h>
 
+int	fit_height(int size, t_sprite *sprite)
+{
+	return ((size * sprite->width / sprite->height));
+}
+
+t_font	*load_char(t_cub3d *cub3d, char *path, char c[2], int font_size)
+{
+	t_font	*font;
+	char	*new_path;
+	int		wid;
+
+	font = (t_font *)malloc(sizeof(t_font));
+	if (!font)
+		return (ft_log("Failed to allocate font mem", NULL, 3), NULL);
+	new_path = ft_calloc(256, sizeof(char));
+	ft_strlcat(new_path, path, 256);
+	ft_strlcat(new_path, c, 256);
+	ft_strlcat(new_path, ".xpm", 256);
+	font->sprite = xpm_load_image(cub3d->mlx, new_path, 1);
+	if (font->sprite)
+	{
+		font->name = c[0];
+		wid = fit_height(font_size, font->sprite);
+		font->sprite = ft_scale_sprite(cub3d, font->sprite, wid, font_size);
+	}
+	else
+		ft_log("Failed to load font character", new_path, 2);
+	free(new_path);
+	return (font);
+}
+
 t_font	**load_font(t_cub3d *cub3d, char *path, int font_size)
 {
 	t_font	**characters;
-	t_font	*font;
 	int		i;
-	char	*new_path;
 	char	c[2];
 
 	characters = (t_font **)malloc(sizeof(t_font *) * 256);
-	new_path = ft_calloc(256, sizeof(char));
 	if (!characters)
-		return (ft_log("Failed to allocate memory for font characters", NULL, 3), NULL);
+		return (ft_log("Failed to allocate memory for font", NULL, 3), NULL);
 	ft_log("Loading font characters", NULL, 1);
 	i = 0;
 	while (i < 256)
@@ -35,33 +63,17 @@ t_font	**load_font(t_cub3d *cub3d, char *path, int font_size)
 		{
 			characters[i] = NULL;
 			i++;
-			continue;
+			continue ;
 		}
 		c[0] = i;
 		c[1] = '\0';
-		ft_strlcat(new_path, path, 256);
-		ft_strlcat(new_path, c, 256);
-		ft_strlcat(new_path, ".xpm", 256);
-		font = (t_font *)malloc(sizeof(t_font));
-		if (!font)
-			return (ft_log("Failed to allocate memory for font character", NULL, 3), NULL);
-		font->sprite = xpm_load_image(cub3d->mlx, new_path, 1);
-		if (font->sprite)
-		{
-			font->name = i;
-			font->sprite = ft_scale_sprite(cub3d, font->sprite, (font_size * font->sprite->width) / font->sprite->height, font_size);
-			characters[i] = font;
-		}
-		else
-			ft_log("Failed to load font character", new_path, 2);
-		new_path = ft_memset(new_path, 0, 256);
+		characters[i] = load_char(cub3d, path, c, font_size);
 		i++;
 	}
-	free(new_path);
 	return (characters);
 }
 
-void	draw_font(t_cub3d *cub3d, char *str, int x, int y, int offset)
+void	draw_font(t_cub3d *cub3d, char *str, int x, int y)
 {
 	int		i;
 	t_font	*font;
@@ -71,16 +83,16 @@ void	draw_font(t_cub3d *cub3d, char *str, int x, int y, int offset)
 	{
 		if (str[i] == ' ')
 		{
-			x += offset;
+			x += OFFSET;
 			i++;
-			continue;
+			continue ;
 		}
 		font = cub3d->characters[(int)str[i]];
 		if (font)
 			draw_sprite_to_HUD(cub3d, font->sprite, x, y);
 		else
 			ft_log("Character not supported!", &str[i], 2);
-		x += font->sprite->width + offset;
+		x += font->sprite->width + OFFSET;
 		i++;
 	}
 }

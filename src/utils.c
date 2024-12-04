ebbe350a1e61e22cc71c1584c65ab9d29acabf77
msolinsk <msolinsk@student.42warsaw.pl>
2024-12-04 12:08:39 +0100

    Fixed norminette in src/utils.c

--- src/utils.c
+++ src/utils.c
@@ -6,19 +6,17 @@
 /*   By: msolinsk <msolinsk@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 20:22:14 by msolinsk          #+#    #+#             */
-/*   Updated: 2024/11/26 12:49:45 by msolinsk         ###   ########.fr       */
+/*   Updated: 2024/12/04 12:08:14 by msolinsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 #include "../include/libs.h"
-
-//	FOR GETTING FPS
-# include <sys/time.h>
+#include <sys/time.h>
 
 unsigned long	rgb_to_hex(int r, int g, int b)
 {
-	return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+	return (((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff));
 }
 
 int	ft_2d_len(void **array)

db9a218e7b78b90362a726ed34a152f9906fdd88
root <root@msolinsk.>
2024-12-19 18:57:27 +0100

    Added support for spaces in maps

--- src/ft_map_check.c
+++ src/ft_map_check.c
@@ -6,7 +6,7 @@
 /*   By: msolinsk <msolinsk@student@42Warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 15:09:19 by msolinsk          #+#    #+#             */
-/*   Updated: 2024/12/17 18:33:31 by msolinsk         ###   ########.fr       */
+/*   Updated: 2024/12/19 18:55:08 by msolinsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,18 @@
 static int	ft_check_neighbour(t_map *map, int x, int y)
 {
 	if (x - 1 < 0 || (map->grid[y][x - 1] != '0' && \
-		map->grid[y][x - 1] != '1') ||
+		map->grid[y][x - 1] != '1' && map->grid[y][x - 1] != ' ') ||
 		y - 1 < 0 || (map->grid[y - 1][x] != '0' && \
-		map->grid[y - 1][x] != '1') || \
+		map->grid[y - 1][x] != '1' && map->grid[y - 1][x] != ' ') || \
 		x + 1 >= (int)ft_strlen(map->grid[y]) || \
 		(map->grid[y][x + 1] != '0' && \
-		map->grid[y][x + 1] != '1') || \
+		map->grid[y][x + 1] != '1' && map->grid[y][x + 1] != ' ') || \
 		y + 1 >= map->height || (map->grid[y + 1][x] != '0' && \
-		map->grid[y + 1][x] != '1'))
+		map->grid[y + 1][x] != '1' && map->grid[y + 1][x] != ' '))
+	{
+		return (1);
+	}
+	return (0);
 	{
 		return (1);
 	}

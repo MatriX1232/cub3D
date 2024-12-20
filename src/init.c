fa801f14c259be32fd24be0556950193ae358c0b
root <root@msolinsk.>
2024-12-20 15:54:22 +0100

    Added invalid keys handling

--- src/init.c
+++ src/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msolinsk <msolinsk@student@42Warsaw.pl>    +#+  +:+       +#+        */
+/*   By: msolinsk <msolinsk@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 22:44:12 by msolinsk          #+#    #+#             */
-/*   Updated: 2024/12/19 23:53:12 by msolinsk         ###   ########.fr       */
+/*   Updated: 2024/12/20 15:47:34 by msolinsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,9 @@ void	ft_set_default_state(t_cub3d *cub3d)
 
 int	ft_init_cub3d(t_cub3d *cub3d, char **argv)
 {
+	cub3d->map = ft_load_map(cub3d, argv[1]);
+	if (!cub3d->map)
+		return (ft_log("Map failed to load", NULL, 3), 1);
 	cub3d->sprites = ft_load_sprites(cub3d);
 	cub3d->hud = xpm_load_image(cub3d->mlx, "textures/hud.xpm", 0);
 	if (!cub3d->sprites || !cub3d->hud)
@@ -34,12 +37,11 @@ int	ft_init_cub3d(t_cub3d *cub3d, char **argv)
 		WIN_WIDTH, WIN_HEIGHT - RENDER_HEIGHT);
 	if (!cub3d->buffer || !cub3d->buffer_hud)
 		return (ft_log("Failed to create buffer", NULL, 3), 1);
-	cub3d->map = ft_load_map(cub3d, argv[1]);
-	if (!cub3d->map)
-		return (ft_log("Map failed to load", NULL, 3), 1);
 	ft_replace_spaces(cub3d->map);
 	if (ft_check_if_map_valid(cub3d->map))
 		return (ft_log("Map is invalid", NULL, 3), 1);
+	if (ft_check_double_keys(argv[1]))
+		return (ft_log("Invalid keys found", NULL, 3), 1);
 	cub3d->player->current_weapon = &cub3d->weapons[1];
 	cub3d->player->pitch = 100;
 	cub3d->player->hp = 100;

7636d80497054e37e4084de3e46dc11579c9715d
MatriX1232 <stormer0209@gmail.com>
2025-02-02 15:20:54 +0100

    Fixed Uninitialized Value and order of loading assets

--- src_bonus/init.c
+++ src_bonus/init.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 22:44:12 by msolinsk          #+#    #+#             */
-/*   Updated: 2025/02/02 13:43:38 by root             ###   ########.fr       */
+/*   Updated: 2025/02/02 15:18:46 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,12 +51,12 @@ int	ft_init_cub3d(t_cub3d *cub3d, char **argv)
 		return (ft_log("Failed to create buffer", NULL, 3), 1);
 	splash_screen(cub3d);
 	ft_replace_spaces(cub3d->map);
-	init_door_system(cub3d);
 	cub3d->anims = ft_load_anims(cub3d);
+	init_door_system(cub3d);
+	ft_set_player_state(cub3d);
 	if (ft_check_if_map_valid(cub3d, cub3d->map))
 		return (ft_log("Map is invalid", NULL, 3), 1);
 	if (cub3d->player->amount != 1)
 		return (ft_log("Map must contain exactly one player", NULL, 3), 1);
-	ft_set_player_state(cub3d);
 	return (0);
 }
